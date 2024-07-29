3b6ee6bb4cc62f338bb55a439b55a9cb3a00e695
Nicolas3484 <gonzaleznico08@gmail.com>
2024-07-29 13:34:03 -0300

    Agregado 5 de 8 t.ps

---
+++ JSON y metodos de Array/datosBici.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const leerJSON = () => {
+    const bicisJSON = fs.readFileSync('./bicicletas.json', 'utf-8') 
+    const bicisJS = JSON.parse(bicisJSON)
+    return bicisJS
+}
+
+
+
+module.exports = {
+    leerJSON
+}
