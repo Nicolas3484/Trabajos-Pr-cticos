bef16bd6399b52e6408871d565efafa7ce540dc3
Nicolas3484 <gonzaleznico08@gmail.com>
2024-07-29 15:19:02 -0300

    Terminado

---
+++ Creando nuestras propias APIS/src/controllers/moviesController.js
@@ -0,0 +1,173 @@
+const path = require('path');
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+const moment = require('moment');
+const fetch = require('node-fetch');
+
+
+//Aqui tienen otra forma de llamar a cada uno de los modelos
+const Movies = db.Movie;
+const Genres = db.Genre;
+const Actors = db.Actor;
+const API = 'http://www.omdbapi.com/?apikey=d4e35e92';
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll({
+            include: ['genre']
+        })
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id,
+            {
+                include : ['genre']
+            })
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            include: ['genre'],
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    },
+    //Aqui debo modificar para crear la funcionalidad requerida
+    'buscar': (req, res) => {
+        const searchTerm = req.body.titulo;
+        Movies.findOne({
+            where: {
+                title: {
+                    [Op.like]: `%${searchTerm}%`
+                }
+            }
+        })
+        .then(movie => {
+            if (movie) {
+                res.render('moviesDetail.ejs', { movie });
+            } else {
+                fetch(`${API}&t=${searchTerm}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.Response === "True") {
+                            const movieFromAPI = {
+                                title: data.Title,
+                                rating: data.imdbRating,
+                                awards: data.Awards,
+                                length: data.Runtime,
+                                release_date: data.Released,
+                            };
+                            res.render('moviesDetail.ejs', { movie: movieFromAPI });
+                        } else {
+                            res.send('No se encontró la película.');
+                        }
+                    })
+                    .catch(error => res.send(error));
+            }
+        })
+        .catch(error => res.send(error));
+    },
+    //Aqui dispongo las rutas para trabajar con el CRUD
+    add: function (req, res) {
+        let promGenres = Genres.findAll();
+        let promActors = Actors.findAll();
+        
+        Promise
+        .all([promGenres, promActors])
+        .then(([allGenres, allActors]) => {
+            return res.render(path.resolve(__dirname, '..', 'views',  'moviesAdd'), {allGenres,allActors})})
+        .catch(error => res.send(error))
+    },
+    create: function (req,res) {
+        Movies
+        .create(
+            {
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+            }
+        )
+        .then(()=> {
+            return res.redirect('/movies')})            
+        .catch(error => res.send(error))
+    },
+    edit: function(req,res) {
+        let movieId = req.params.id;
+        let promMovies = Movies.findByPk(movieId,{include: ['genre','actors']});
+        let promGenres = Genres.findAll();
+        let promActors = Actors.findAll();
+        Promise
+        .all([promMovies, promGenres, promActors])
+        .then(([Movie, allGenres, allActors]) => {
+            Movie.release_date = moment(Movie.release_date).format('L');
+            return res.render(path.resolve(__dirname, '..', 'views',  'moviesEdit'), {Movie,allGenres,allActors})})
+        .catch(error => res.send(error))
+    },
+    update: function (req,res) {
+        let movieId = req.params.id;
+        let releaseDate = req.body.release_date;
+    if (releaseDate && releaseDate.includes('-')) {
+        const [day, month, year] = releaseDate.split('-');
+        releaseDate = `${year}-${month}-${day}`;
+    }
+        Movies.update(
+            {
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+            },
+            {
+                where: {id: movieId}
+            })
+        .then(()=> {
+            return res.redirect('/movies')})            
+        .catch(error => res.send(error))
+    },
+    delete: function (req,res) {
+        let movieId = req.params.id;
+        Movies
+        .findByPk(movieId)
+        .then(Movie => {
+            return res.render(path.resolve(__dirname, '..', 'views',  'moviesDelete'), {Movie})})
+        .catch(error => res.send(error))
+    },
+    destroy: function (req,res) {
+        let movieId = req.params.id;
+        Movies
+        .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
+        .then(()=>{
+            return res.redirect('/movies')})
+        .catch(error => res.send(error)) 
+    }
+}
+
+module.exports = moviesController;
